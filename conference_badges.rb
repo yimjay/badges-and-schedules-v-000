6341432995eb961c6137c8352a02e1e4ae90edc2
Ji Yim <gsooyim@gmail.com>
2018-04-14 20:00:08 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -15,18 +15,10 @@ end
 # end
 
 def assign_rooms(names)
-  room_assignments = []
-  names.each_with_index do |name, index|
-    room_assignments[index] = "Hello, #{name}! You'll be assigned to room #{index + 1}!"
+  names.each_with_index.map do |name, index|
+    "Hello, #{name}! You'll be assigned to room #{index + 1}!"
   end
-  room_assignments
 end
-
-# def assign_rooms(attendees)
-#   attendees.each_with_index.map do |attendee, index|
-#     "Hello, #{attendee}! You'll be assigned to room #{index+1}!"
-#   end
-# end
 
 def printer(attendees)
   batch_badge_creator(attendees).each do |element|
