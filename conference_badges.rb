cd57d6108f099804e922ee0344c71e9983a0a040
Ji Yim <gsooyim@gmail.com>
2018-04-14 19:59:24 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -22,6 +22,12 @@ def assign_rooms(names)
   room_assignments
 end
 
+# def assign_rooms(attendees)
+#   attendees.each_with_index.map do |attendee, index|
+#     "Hello, #{attendee}! You'll be assigned to room #{index+1}!"
+#   end
+# end
+
 def printer(attendees)
   batch_badge_creator(attendees).each do |element|
     puts "#{element}"
